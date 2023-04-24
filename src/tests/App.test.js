2e514efd363cda93c7340fc51d43d75c72ec1f00
Wlauber <wlaubersamuel@gmail.com>
2023-04-24 19:59:07 -0300

    help

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -53,5 +53,47 @@ describe('Testando o componente <App />', () => {
     act(() => {
       fireEvent.click(allButtons2[3]);
     });
+
+    //filtros combinados
+    act(() => {
+      // first filter
+      const select = screen.getByTestId('column-filter');
+      fireEvent.change(select, { target: { value: 'population' } });
+
+      const selectComparison = screen.getByTestId('comparison-filter');
+      fireEvent.change(selectComparison, { target: { value: 'maior que' } });
+
+      const inputNumber = screen.getByTestId('value-filter');
+      fireEvent.change(inputNumber, { target: { value: '100000' } });
+
+      const button = screen.getByRole('button', { name: /filtrar/i });
+      fireEvent.click(button);
+
+      // second filter
+      const select2 = screen.getByTestId('column-filter');
+      fireEvent.change(select2, { target: { value: 'orbital_period' } });
+
+      const selectComparison2 = screen.getByTestId('comparison-filter');
+      fireEvent.change(selectComparison2, { target: { value: 'menor que' } });
+
+      const inputNumber2 = screen.getByTestId('value-filter');
+      fireEvent.change(inputNumber2, { target: { value: '400' } });
+
+      const button2 = screen.getByRole('button', { name: /filtrar/i });
+      fireEvent.click(button2);
+
+      // third filter
+      const select3 = screen.getByTestId('column-filter');
+      fireEvent.change(select3, { target: { value: 'surface_water' } });
+
+      const selectComparison3 = screen.getByTestId('comparison-filter');
+      fireEvent.change(selectComparison3, { target: { value: 'igual a' } });
+
+      const inputNumber3 = screen.getByTestId('value-filter');
+      fireEvent.change(inputNumber3, { target: { value: '12' } });
+
+      const button3 = screen.getByRole('button', { name: /filtrar/i });
+      fireEvent.click(button3);
+    });
   });
 });
